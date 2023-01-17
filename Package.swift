1bb83b616b6ef00e90be1c0f48e09a388588c2ac
Stasel <2033301+stasel@users.noreply.github.com>
2023-01-17 23:35:45 +0100

    Updated files for release M109.0.1

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/stasel/WebRTC/releases/download/109.0.0/WebRTC-M109.xcframework.zip",
-            checksum: "24f22b7a283e4c37daec64cf030fc356dff27d58e37eb0a604a084b28c5b06d8"
+            url: "https://github.com/stasel/WebRTC/releases/download/109.0.1/WebRTC-M109.0.1.xcframework.zip",
+            checksum: "28f5fa694cc9ccc36eb5b7ac19305e1fbf0ade03eabfd02cbc0386cda693c9f1"
         ),
     ]
 )
