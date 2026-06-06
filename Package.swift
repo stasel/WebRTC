af2bdbfa111f3ecab51d233df8c59209d27c3e6c
Anka <runner@sjc22-bm209-369b09db-fc79-40fd-b754-f49858a34d14-0A51468F6F9C.local>
2026-06-06 15:04:17 +0000

    Updated files for release M149

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/stasel/WebRTC/releases/download/147.0.0/WebRTC-M147.xcframework.zip",
-            checksum: "49f9b1713432c19f408e3218fc8526c7692fafca5869f7ec5f5991614276ed40"
+            url: "https://github.com/stasel/WebRTC/releases/download/149.0.0/WebRTC-M149.xcframework.zip",
+            checksum: "9726f1db0c99e69806257a7166fb16f79a41035240384a04803902a509f0fc8c"
         ),
     ]
 )
