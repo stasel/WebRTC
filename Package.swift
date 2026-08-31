3e43fa9b8d149d2f8adb9bb1e53152cb955ec72b
stasel <2033301+stasel@users.noreply.github.com>
2026-08-31 14:52:08 +0200

    Updated new checksum hash for M151 re-release

--- Package.swift
+++ Package.swift
@@ -7,14 +7,15 @@ let package = Package(
     products: [
         .library(
             name: "WebRTC",
-            targets: ["WebRTC"]),
+            targets: ["WebRTC"])
     ],
-    dependencies: [ ],
+    dependencies: [],
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/stasel/WebRTC/releases/download/151.0.0/WebRTC-M151.xcframework.zip",
-            checksum: "64a218fad3d84a0d783321aa9a1eec58ca266ac7879123f86b0b44b703b7d8dc"
-        ),
+            url:
+                "https://github.com/stasel/WebRTC/releases/download/151.0.0/WebRTC-M151.xcframework.zip",
+            checksum: "6f3f5693383ce65763190c46ca9f2c4325c34b83681acb9db30f01488e15f1e0"
+        )
     ]
 )
